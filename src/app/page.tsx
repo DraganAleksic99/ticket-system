41762a5c81222b8b629abc5bef843cf0f74baee2
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-12-19 18:51:27 +0100

    update: add basic login component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
-"use client";
+import { Login } from "./_components/login";
+import { SearchParams } from "next/dist/server/request/search-params";
 
-import { useEffect } from "react";
-import { getSupabaseBrowserClient } from "./supabase-utils/browser-client";
+export default function Home({ searchParams }: { searchParams: SearchParams }) {
+  const megicLinkParamExists = searchParams.magicLink === "yes";
 
-export default function Home() {
-  useEffect(() => {
-    const client = getSupabaseBrowserClient();
-    client.storage
-      .listBuckets()
-      .then((result) => console.log("Bucket list", result));
-  }, []);
-
-  return <div></div>;
+  return <Login isPasswordLogin={!megicLinkParamExists} />;
 }
