8964fb2fecae1c3eedc7b3f7b1e51bd509252534
Dragan Aleksic <aleksicdragan574@gmail.com>
2025-01-02 21:22:09 +0100

    update: fetch ticket data in ticket details page

--- src/app/[tenant]/tickets/details/[id]/page.tsx
+++ src/app/[tenant]/tickets/details/[id]/page.tsx
@@ -1,24 +1,41 @@
+import { notFound } from "next/navigation";
 import { SearchParams } from "next/dist/server/request/search-params";
 import { TicketComments } from "@/app/_components/ticket-comments";
+import { getSupabaseCookiesUtilClient } from "@/app/supabase-utils/cookies-util-client";
+import { TICKET_STATUS } from "@/app/utils/constants";
 
-export default function TicketDetailsPage({
+export default async function TicketDetailsPage({
   params,
 }: {
   params: SearchParams;
 }) {
+  const supabase = await getSupabaseCookiesUtilClient();
+  const { id } = await params;
+
+  const { data: ticket, error } = await supabase
+    .from("tickets")
+    .select("*")
+    .eq("id", Number(id))
+    .single();
+
+  if (error) return notFound();
+
+  const { created_at, title, description, status, author_name } = ticket;
+
+  const dateString = new Date(created_at).toLocaleString("en-US");
+
   return (
     <article className="ticketDetails">
       <header>
-        <strong>#{params.id}</strong> -{" "}
-        <strong className="ticketStatusGreen">Open</strong>
+        <strong>#{id}</strong> -{" "}
+        <strong className="ticketStatusGreen">{TICKET_STATUS[status]}</strong>
         <br />
         <small className="authorAndDate">
-          Created by <strong>AuthorName</strong> at{" "}
-          <time>December 10th 2025</time>
+          Created by <strong>{author_name}</strong> at <time>{dateString}</time>
         </small>
-        <h2>Ticket title should be here</h2>
+        <h2>{title}</h2>
       </header>
-      <section>Some details about the ticket should be here.</section>
+      <section>{description}</section>
       <TicketComments />
     </article>
   );
